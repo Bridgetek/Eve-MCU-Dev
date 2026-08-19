3aa30d2423ee42db2dbd9c8a52013e848e131e0d
Aaron Jones <aaron.jones@brtchip.com>
2026-08-19 19:36:13 +0100

    Update comments for conformity, after include update for #52

--- ports/eve_emulator/EVE_emulator.c
+++ ports/eve_emulator/EVE_emulator.c
@@ -2,47 +2,47 @@
  * @file EVE_emulator.c
  * @details Platform-specific code for controlling running a EVE Emulator instance.
  */
- /*
-  * ============================================================================
-  * (C) Copyright,  Bridgetek Pte. Ltd.
-  * ============================================================================
-  *
-  * This source code ("the Software") is provided by Bridgetek Pte Ltd
-  * ("Bridgetek") subject to the licence terms set out
-  * http://brtchip.com/BRTSourceCodeLicenseAgreement/ ("the Licence Terms").
-  * You must read the Licence Terms before downloading or using the Software.
-  * By installing or using the Software you agree to the Licence Terms. If you
-  * do not agree to the Licence Terms then do not download or use the Software.
-  *
-  * Without prejudice to the Licence Terms, here is a summary of some of the key
-  * terms of the Licence Terms (and in the event of any conflict between this
-  * summary and the Licence Terms then the text of the Licence Terms will
-  * prevail).
-  *
-  * The Software is provided "as is".
-  * There are no warranties (or similar) in relation to the quality of the
-  * Software. You use it at your own risk.
-  * The Software should not be used in, or for, any medical device, system or
-  * appliance. There are exclusions of Bridgetek liability for certain types of loss
-  * such as: special loss or damage; incidental loss or damage; indirect or
-  * consequential loss or damage; loss of income; loss of business; loss of
-  * profits; loss of revenue; loss of contracts; business interruption; loss of
-  * the use of money or anticipated savings; loss of information; loss of
-  * opportunity; loss of goodwill or reputation; and/or loss of, damage to or
-  * corruption of data.
-  * There is a monetary cap on Bridgetek's liability.
-  * The Software may have subsequently been amended by another user and then
-  * distributed by that other user ("Adapted Software").  If so that user may
-  * have additional licence terms that apply to those amendments. However, Bridgetek
-  * has no liability in relation to those amendments.
-  * ============================================================================
-  */
+/*
+ * ============================================================================
+ * (C) Copyright,  Bridgetek Pte. Ltd.
+ * ============================================================================
+ *
+ * This source code ("the Software") is provided by Bridgetek Pte Ltd
+ * ("Bridgetek") subject to the licence terms set out
+ * http://brtchip.com/BRTSourceCodeLicenseAgreement/ ("the Licence Terms").
+ * You must read the Licence Terms before downloading or using the Software.
+ * By installing or using the Software you agree to the Licence Terms. If you
+ * do not agree to the Licence Terms then do not download or use the Software.
+ *
+ * Without prejudice to the Licence Terms, here is a summary of some of the key
+ * terms of the Licence Terms (and in the event of any conflict between this
+ * summary and the Licence Terms then the text of the Licence Terms will
+ * prevail).
+ *
+ * The Software is provided "as is".
+ * There are no warranties (or similar) in relation to the quality of the
+ * Software. You use it at your own risk.
+ * The Software should not be used in, or for, any medical device, system or
+ * appliance. There are exclusions of Bridgetek liability for certain types of loss
+ * such as: special loss or damage; incidental loss or damage; indirect or
+ * consequential loss or damage; loss of income; loss of business; loss of
+ * profits; loss of revenue; loss of contracts; business interruption; loss of
+ * the use of money or anticipated savings; loss of information; loss of
+ * opportunity; loss of goodwill or reputation; and/or loss of, damage to or
+ * corruption of data.
+ * There is a monetary cap on Bridgetek's liability.
+ * The Software may have subsequently been amended by another user and then
+ * distributed by that other user ("Adapted Software").  If so that user may
+ * have additional licence terms that apply to those amendments. However, Bridgetek
+ * has no liability in relation to those amendments.
+ * ============================================================================
+ */
 
-  // Guard against being used for incorrect platform or architecture.
-  // PLATFORM_EMULATOR macro enables this file to open the emulator library.
-  // In gcc compilers this is in the Makefile. -DPLATFORM_EMULATOR
-  // In Visual Studio this is in Project Properties -> Configuration Properties -> 
-  //     C/C++ -> Preprocessor -> Preprocessor Definitions.
+// Guard against being used for incorrect platform or architecture.
+// PLATFORM_EMULATOR macro enables this file to open the emulator library.
+// In gcc compilers this is in the Makefile. -DPLATFORM_EMULATOR
+// In Visual Studio this is in Project Properties -> Configuration Properties -> 
+//     C/C++ -> Preprocessor -> Preprocessor Definitions.
 #if defined(PLATFORM_EMULATOR)
 
 #pragma message ("Compiling " __FILE__ " for the emulator")
@@ -66,6 +66,7 @@
 #define HOST_IS_LITTLE_ENDIAN 0
 #endif
 
+// Include EVE_Emulator header files
 #include "EVE_Emulator/include/bt8xxemu_inttypes.h"
 #include "EVE_Emulator/include/bt8xxemu.h"
 
@@ -84,12 +85,14 @@
 
 // This platform specific section contains the functions which create the EVE Emulator instance
 
+// Emulator state
 static BT8XXEMU_Emulator* Emulator = NULL;
 static BT8XXEMU_EmulatorParameters* EmulatorParameters = NULL;
 
 static BT8XXEMU_Flash* EmulatorFlash = NULL;
 static BT8XXEMU_FlashParameters* EmulatorFlashParameters = NULL;
 
+// Type used when resolving emulator SD Folder or Flash Image path.
 typedef enum
 {
     MCU_PATH_FILE,
